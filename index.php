5a817e7b2a1aebd3ce433bfbf68fa832a3f8b5fa
diphadjanotamaa <allendd0211@gmail.com>
2024-10-20 11:05:13 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sinkronisasi folder lokal dengan Github</title>
+</head>
+<body>
+    <h1>ini adalah Tugas 1</h1>
+</body>
+</html>
